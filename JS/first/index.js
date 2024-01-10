9c1815635b7a5c5d97b41392e22a72b19021ee3b
prasad-bigdp <bigdp585@gmail.com>
2024-01-10 13:59:11 +0530

    update

---
+++ JS/first/index.js
@@ -0,0 +1,80 @@
+// /* multi line comment */
+// //single line comment
+
+
+
+// // alert("application successfully completely")
+// let num1 = prompt("enter a value") //show one box and it will ask value, 5 =>'5'
+// console.log(typeof(num1))
+// let num2 = prompt("enter a value")
+// //6=>'6'
+
+// console.log(num1+num2)//'5'+'6'
+// console.log("my first javascript")
+
+
+/* variables*/
+
+/*var, let and const */
+/* var till 2015(ES6), let and const */
+/* var:*/
+// console.log(a) //Hoisting
+// var a = 20;
+// var a = 50;
+// let b = 20;
+// b = 50;
+// console.log(b)
+
+// const c = 20;
+
+// console.log(c)
+
+
+
+
+
+// var a = 55;
+// console.log(a)
+
+// function add ()
+// {
+//     if (true)
+//     {
+//       var pt =50;
+//     }   
+//     console.log(pt)
+// }
+// add()
+// debugger;
+// console.log(p1)
+
+// var p1 = 55;
+// let a = 63;
+// const c = 65;
+// console.log(a, c)
+// function fun ()
+// {
+//     console.log(b)
+//     var b = 55;
+//     console.log(b)
+// }
+// fun()
+/* numbers */
+
+let a = 55;
+
+console.log(typeof(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
