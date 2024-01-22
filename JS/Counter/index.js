7e3d3f43483043330c2d7d27ed98f0b77cf9c801
prasad-bigdp <bigdp585@gmail.com>
2024-01-22 15:35:38 +0530

    update

---
+++ JS/Counter/index.js
@@ -0,0 +1,43 @@
+const count = document.querySelector('#counter>p')
+let c = 0
+console.log(count.textContent)
+// function incr() {
+// 	console.log("increement called")
+// 	if (c >= 25) {
+// 		alert("number should not be greater than 25")
+//     }
+//     else
+//     {
+//        c++
+// 				count.textContent = c
+//     }
+		
+	
+// }
+// function decr() {
+// 	console.log("decrement called")
+// 	if (c <= 0) {
+// 		alert("number should not be decreased less than zero")
+// 	} else {
+// 		c--
+// 		count.textContent = c
+// 	}
+// }
+// function reset() {
+// 	console.log("reset called")
+// 	count.textContent = 0
+// }
+function inc1 ()
+{
+    console.log("inc1 called")
+	c=c+2
+ count.textContent=c
+}
+function inc2() {
+	c += 3
+	count.textContent=c
+}
+function inc3() {
+	c += 4
+count.textContent=c
+}
---
+++ JS/DOM/script.js
@@ -0,0 +1,13 @@
+const para = document.querySelectorAll('.para')
+console.log(para)
+// console.log(para.textContent)
+// console.log(para.innerText)
+// para.textContent="msdbhm"
+
+// para[0]].textContent = "Hello World"
+// console.log(para[0].textContent)
+
+// para[0].innerText = "Bye World"
+// console.log(para[0].innerText)
+// para[0].innerHTML= `<strong>My World</strong>`
+/**   take paragraph from  html and change the text using textContent ,innerText and innerHTML */
